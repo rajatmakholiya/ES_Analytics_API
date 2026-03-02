6a9b3d10413b52dfa56289553745ea4414d32887
rajatmakholiya <rajatmakholiya07@gmail.com>
2026-03-02 16:02:08 +0530

    cors error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, 
+    origin: ['https://es-analytics-dash-sand.vercel.app',], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
